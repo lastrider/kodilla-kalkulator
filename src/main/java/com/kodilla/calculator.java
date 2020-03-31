899e0d0b698966009bb23733cd5e19a7a1ea6279
lastrider <l.leonowicz@gmail.com>
2020-03-31 18:55:52 +0200

    2

--- src/main/java/com/kodilla/calculator.java
+++ src/main/java/com/kodilla/calculator.java
@@ -1,4 +1,12 @@
 package com.kodilla;
 
 public class calculator {
+
+    public int addition(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+       public int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
 }
